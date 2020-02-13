25d96abacb102bdbc042c70c63b1e6e1c1127f4d
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-02-14 00:26:53 +0100

    probably don't need this stuff

--- shell.nix
+++ shell.nix
@@ -3,8 +3,4 @@ pkgs.mkShell {
   buildInputs = [
     pkgconfig openssl cmake zlib libgit2 python llvm_7 libxml2
   ];
-  shellHook = ''
-    export TMPDIR=/tmp
-    export RUSTC_BOOTSTRAP=1
-  '';
 }
