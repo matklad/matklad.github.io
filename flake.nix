8bb566d12917e9819dc2790786baf3f484a5c83f
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-11-28 18:39:07 +0300

    .

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [ pkg-config zlib openssl bundler ];
+      };
+    });
+}
