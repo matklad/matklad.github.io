c6769f147f0f75fc7e508a98d2dc0646006d5013
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-07-10 20:00:25 +0100

    almost rules

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 # rm ./vendor -rf && nix-shell --run 'bundle install'
-# nix-shell --run 'bundle exec jekyll serve --livereload --quiet'
+# nix-shell --run 'bundle exec jekyll serve --livereload'
 with import <nixpkgs> {}; mkShell {
   packages = [ ruby ];
 }
