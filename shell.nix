2d99e25c057bcefc0d7d71894b43b45f3a82dc0b
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-11-12 21:51:16 +0000

    remove adoc

--- shell.nix
+++
@@ -1,7 +0,0 @@
-with import <nixpkgs> {}; mkShell {
-  packages = [ 
-    (writeShellScriptBin "serve" "bundle exec jekyll serve --livereload")
-    (writeShellScriptBin "install" "rm ./vendor -rf && bundle install")
-    ruby libffi pkg-config
-  ];
-}
